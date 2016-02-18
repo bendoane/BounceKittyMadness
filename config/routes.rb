2a048c77256823289c6ad61ec549186e3beb39cc
bendoane <ben.doane@gmail.com>
2016-02-18 16:31:48 -0500

    new users, beginning authlogic

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
    root 'users#index'
    get 'not21' => 'users#not21'
+   resources :users, :only => [:new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
